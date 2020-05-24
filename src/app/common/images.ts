dac9bb51ba75810af0faeba8c7c763276ea08c04
Denis Tatarenko <denis.t@keymakr.com>
2020-05-24 15:01:14 +0300

    DD-5: update webpack to not trying to run СhromeReloader plugin on prod build

---
+++ src/app/common/images.ts
@@ -0,0 +1,26 @@
+export const JIRA = `<?xml version="1.0" encoding="utf-8"?>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     viewBox="0 0 73.3 75.8" style="enable-background:new 0 0 73.3 75.8;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#2684FF;}
+	.st1{fill:url(#SVGID_1_);}
+	.st2{fill:url(#SVGID_2_);}
+</style>
+    <title>Jira Software-blue</title>
+    <g>
+	<path class="st0" d="M72.4,35.8L39.8,3.2L36.6,0l0,0L12.1,24.5l0,0L0.9,35.8c-1.2,1.2-1.2,3.1,0,4.2l22.4,22.4l13.3,13.3l24.5-24.5
+		l0.4-0.4L72.4,40C73.6,38.8,73.6,36.9,72.4,35.8z M36.6,49.1L25.4,37.9l11.2-11.2l11.2,11.2L36.6,49.1z"/>
+
+        <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="34.6324" y1="61.935" x2="18.9924" y2="46.295" gradientTransform="matrix(1 0 0 -1 0 77.28)">
+		<stop  offset="0.18" style="stop-color:#0052CC"/>
+            <stop  offset="1" style="stop-color:#2684FF"/>
+	</linearGradient>
+        <path class="st1" d="M36.6,26.7c-7.3-7.3-7.4-19.2-0.1-26.6L12,24.6l13.3,13.3L36.6,26.7z"/>
+
+        <linearGradient id="SVGID_2_" gradientUnits="userSpaceOnUse" x1="38.7774" y1="17" x2="54.3874" y2="32.61" gradientTransform="matrix(1 0 0 -1 0 77.28)">
+		<stop  offset="0.18" style="stop-color:#0052CC"/>
+            <stop  offset="1" style="stop-color:#2684FF"/>
+	</linearGradient>
+        <path class="st2" d="M47.9,37.8L36.6,49.1c7.4,7.4,7.4,19.3,0,26.7c0,0,0,0,0,0l0,0l24.6-24.6L47.9,37.8z"/>
+</g>
+</svg>`;
